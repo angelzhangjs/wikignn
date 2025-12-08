290cf13daca96da875c6996e4dd78fee46d3d0a6
angelzhangjs <cs.research.ai@gmail.com>
2025-12-08 12:02:38 +0000

    clean code

--- retrieval.sh
+++ retrieval.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# python3 -m retrieval.build_candidates_from_graph \
-#   --graph-pt /home/ghr/angel/gnn/graph_embedding/graph_by_label.no_literal.pyg.pt.with_clip.pt \
-#   --out-dir /home/ghr/angel/gnn/candidates \
-#   --seed-node-type entity \
-#   --num-hops 2 --num-neighbors 16 --max-candidates 20000 --batch-size 1
-
 python3 -m retrieval.infer_and_answer \
   --checkpoint /home/ghr/angel/gnn/training_output/best.pth \
   --candidates-dir /home/ghr/angel/gnn/candidates \
