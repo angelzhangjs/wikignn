35e2c7e6480247fcdc4ce1e7265e5f7fd54e5adb
angelzhangjs <cs.research.ai@gmail.com>
2025-12-08 11:47:52 +0000

    gnn

--- retrieval.sh
+++ retrieval.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # python3 -m retrieval.build_candidates_from_graph \
 #   --graph-pt /home/ghr/angel/gnn/graph_embedding/graph_by_label.no_literal.pyg.pt.with_clip.pt \
 #   --out-dir /home/ghr/angel/gnn/candidates \
