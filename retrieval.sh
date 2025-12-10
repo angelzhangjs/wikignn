973ea91f888a13a965121d5e07605e4d561a9043
angelzhangjs <cs.research.ai@gmail.com>
2025-12-10 01:27:48 +0000

    add api toknen

--- retrieval.sh
+++ retrieval.sh
@@ -4,5 +4,5 @@ python3 -m retrieval.infer_and_answer \
   --checkpoint /home/ghr/angel/gnn/training_output/best.pth \
   --candidates-dir /home/ghr/angel/gnn/candidates \
   --query "Tell me some artwork created by Renaissance Italian painter and polymath" --topk 50 --gemini \
-  --gemini-api-key "AIzaSyBlTEJcSEQe8L-g8ZEjEZWBgY9Xb1qDLB0" \
+  --gemini-api-key "AIzaSyBUPSu6K_hK2-O-YyHT-Tc9Gx5lVDPhwYI" \
   --gemini-model "gemini-2.5-flash"
